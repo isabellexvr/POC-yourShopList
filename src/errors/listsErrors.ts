60a69d76a240ab67009f9e77d85b5310042d7802
isabellexvr <isabellexavier59@gmail.com>
2023-01-23 13:27:19 -0300

    refactor: error function + type error

--- src/errors/listsErrors.ts
+++ src/errors/listsErrors.ts
@@ -1,5 +1,9 @@
 import { ServerError } from "../protocols/errorProtocols";
 
-export function listNotFoundError(){
+export function listNameConflictError(): ServerError{
     return { name: "list_name_error", message: "this list name is already being used" }
+}
+
+export function listNotFoundError(): ServerError{
+    return { name: "no_lists_found", message: "list not found or not attached to this user" };
 }
--- src/services/listsServices.ts
+++ src/services/listsServices.ts
@@ -1,13 +1,14 @@
-import { listNotFoundError } from '../errors/listsErrors';
+import { listNameConflictError, listNotFoundError } from '../errors/listsErrors';
 import { List, ItemEntity, ListEntity } from '../protocols/listsProtocols';
 import { QueryResult } from 'pg';
 import listsRepository from '../repositories/listsRepository';
+
 
 async function checkListExistence(listName: string, userId: number): Promise<QueryResult<List>> {
 
     const checkListName: QueryResult<List> = await listsRepository.getlistByUserId(listName, userId);
 
-    if (checkListName.rows.length > 0) throw listNotFoundError();
+    if (checkListName.rows.length > 0) throw listNameConflictError();
 
     return checkListName;
 }
@@ -26,17 +27,18 @@ async function checkItemExistence(itemName: string): Promise<number> {
 
 async function checkIfListBelongsToUser(userId: number, listId: number): Promise<number> {
 
-    const list: QueryResult<ListEntity> = await listsRepository.getListByListId(userId, listId)
+    const list: QueryResult<ListEntity> = await listsRepository.getListByListId(userId, listId);
 
-    if (list.rows.length <= 0) throw { name: "no_lists_found", message: "list not found or not attached to this user" };
-    return list.rows[0].id
+    if (list.rows.length <= 0) throw listNotFoundError();
 
-}
+    return list.rows[0].id;
+
+};
 
 const listsServices = {
     checkListExistence,
     checkItemExistence,
     checkIfListBelongsToUser
-}
+};
 
-export default listsServices
+export default listsServices;
